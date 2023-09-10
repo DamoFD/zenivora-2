e1535032b5abaf63be8b4f57301b58cecaa8b7a7
DamoFD <voshall@yahoo.com>
2023-09-10 16:26:47 -0700

    added second star animation
    added second star animation

---
+++ resources/views/components/about.blade.php
@@ -0,0 +1,4 @@
+<section class="relative" id="about">
+    <h2 class="text-3xl">Harness the power of next-gen website technology</h2>
+    <canvas class="absolute h-screen w-full bg-brand-gray top-0 left-0" id="canvas-2"></canvas>
+</section>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
     <x-hero />
+    <x-about />
     <script src="{{ asset('js/star-animation.js') }}"></script>
+    <script src="{{ asset('js/no-connect-animation.js') }}"></script>
 </x-app-layout>
