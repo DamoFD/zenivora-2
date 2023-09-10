78b4eda97dba2c8c4492ab704217c60d8077fba8
DamoFD <voshall@yahoo.com>
2023-09-09 23:17:37 -0700

    refactor js and add css animations
    refactor js and add css animations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,113 +14,34 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
+        <section class="w-full h-screen justify-center items-center flex flex-col" id="hero">
         <canvas class="fixed h-screen w-full bg-[#232323] top-0 left-0" id="canvas"></canvas>
-<script>
-    window.onload = function() {
-var canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext('2d');
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-var stars = [], // Array that contains the stars
-    FPS = 60, // Frames per second
-    x = 100, // Number of stars
-    mouse = {
-      x: 0,
-      y: 0
-    };  // mouse location
-
-// Push stars to array
-
-for (var i = 0; i < x; i++) {
-  stars.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    radius: Math.random() * 1 + 1,
-    vx: Math.floor(Math.random() * 50) - 25,
-    vy: Math.floor(Math.random() * 50) - 25
-  });
-}
-
-// Draw the scene
-
-function draw() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-
-  ctx.globalCompositeOperation = "lighter";
-
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
-
-    ctx.fillStyle = "#465675";
-    ctx.beginPath();
-    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.fillStyle = 'black';
-    ctx.stroke();
-  }
-
-  ctx.beginPath();
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var starI = stars[i];
-    ctx.moveTo(starI.x,starI.y);
-    if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
-    for (var j = 0, x = stars.length; j < x; j++) {
-      var starII = stars[j];
-      if(distance(starI, starII) < 150) {
-        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
-        ctx.lineTo(starII.x,starII.y);
-      }
-    }
-  }
-  ctx.lineWidth = 0.1;
-  ctx.strokeStyle = '#465675';
-  ctx.stroke();
-}
-
-function distance( point1, point2 ){
-  var xs = 0;
-  var ys = 0;
-
-  xs = point2.x - point1.x;
-  xs = xs * xs;
-
-  ys = point2.y - point1.y;
-  ys = ys * ys;
-
-  return Math.sqrt( xs + ys );
-}
-
-// Update star locations
-
-function update() {
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
-
-    s.x += s.vx / FPS;
-    s.y += s.vy / FPS;
-
-    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
-  }
-}
-
-canvas.addEventListener('mousemove', function(e){
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
-});
-
-// Update and draw
-
-function tick() {
-  draw();
-  update();
-  requestAnimationFrame(tick);
-}
-
-tick();
-};
-</script>
+        <h1 class="z-[2] font-extrabold text-5xl text-blue-300 uppercase">Zenivora</h1>
+        <h3 class="span loader uppercase">
+        <span>R</span>
+        <span>e</span>
+        <span>n</span>
+        <span>o</span>
+        <span>&nbsp;</span>
+        <span>W</span>
+        <span>e</span>
+        <span>b</span>
+        <span>s</span>
+        <span>i</span>
+        <span>t</span>
+        <span>e</span>
+        <span>&nbsp;</span>
+        <span>D</span>
+        <span>e</span>
+        <span>s</span>
+        <span>i</span>
+        <span>g</span>
+        <span>n</span>
+        <span>e</span>
+        <span>r</span>
+        <span>s</span>
+        </h3>
+        </section>
+<script src="{{ asset('js/star-animation.js') }}"></script>
     </body>
 </html>
