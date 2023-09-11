64d9a2330e3406cc42ef8eb8c64f7cb2d967d107
DamoFD <voshall@yahoo.com>
2023-09-11 14:12:45 -0700

    created technology section
    created technology section

---
+++ resources/views/components/technology.blade.php
@@ -0,0 +1,38 @@
+<section class="w-full h-screen flex justify-center items-center flex-col" id="technology">
+    <h2 class="text-white text-3xl font-mont font-extrabold">Harness the power of <strong class="bg-gradient-to-r from-brand-primary to-blue-400 text-transparent bg-clip-text">next-gen</strong> website technology</h2>
+    <div class="grid w-3/4 grid-cols-3 gap-6 mt-10">
+        <x-gradient-border-card>
+            <img class="w-12 h-12 mb-4 self-center" src="{{ asset('svg/marker.svg') }}" alt="location marker" />
+            <h3 class="text-white text-xl font-mont font-extrabold text-center">Located in Reno, NV</h3>
+            <p class="text-white text-lg font-roboto mt-4">You may have noticed how hard it is to find software engineers that are <em>actually</em> located in the Reno area.</p>
+            <p class="text-white text-lg font-roboto mt-4">Zenivora is a <strong>local</strong> business here in Reno, NV!</p>
+            <ul class="list-disc">
+                <li class="text-white text-lg font-roboto mt-4">Access to strong local market research.</li>
+                <li class="text-white text-lg font-roboto mt-4">Take advantage of high-tier connections in the Reno, NV area.</li>
+                <li class="text-white text-lg font-roboto mt-4">Local Reno, NV business that contributes to massive codebases including the <a class="text-brand-primary underline" href="https://wordpress.org">Wordpress Core</a> and the <a class="text-brand-primary underline" href="https://phpmyadmin.net">PhpMyAdmin database interface</a>.</li>
+            </ul>
+        </x-gradient-border-card>
+        <x-gradient-border-card>
+            <img class="w-12 h-12 mb-4 self-center" src="{{ asset('svg/seo.svg') }}" alt="seo icon" />
+            <h3 class="text-white text-xl font-mont font-extrabold text-center">Rank #1 on Google</h3>
+            <p class="text-white text-lg font-roboto mt-4">Web agencies love to promise the world. Look at how many <em>low-quality, low-performant</em> sites there are in Reno, NV.</p>
+            <p class="text-white text-lg font-roboto mt-4">Here at Zenivora, you are <strong>guaranteed</strong> a high-quality site that <em>will</em> rank.</p>
+            <ul class="list-disc">
+                <li class="text-white text-lg font-roboto mt-4">You will receive powerful keyword research with <a class="text-brand-primary underline" href="https://ahrefs.com">Ahrefs</a>.</li>
+                <li class="text-white text-lg font-roboto mt-4">Gain the SERPs with keyword rich articles with <a class="text-brand-primary underline" href="https://surferseo.com">Surfer SEO</a>.</li>
+                <li class="text-white text-lg font-roboto mt-4">Increase visibility with high authority and relevant backlinks</li>
+            </ul>
+        </x-gradient-border-card>
+        <x-gradient-border-card>
+            <img class="w-12 h-12 mb-4 self-center" src="{{ asset('svg/development.svg') }}" alt="development" />
+            <h3 class="text-white text-xl font-mont font-extrabold text-center">Industry Standard Development</h3>
+            <p class="text-white text-lg font-roboto mt-4">The developers at Zenivora have experience working with <strong>thousands</strong> of other developers on huge codebases.</p>
+            <p class="text-white text-lg font-roboto mt-4">Your software has the <em>ability</em> to run your business for you.</p>
+            <ul class="list-disc">
+                <li class="text-white text-lg font-roboto mt-4">Your software will manage your customers, employees, books, and inventory.</li>
+                <li class="text-white text-lg font-roboto mt-4">Withstand huge influxes of requests and traffic with proper optimizations.</li>
+                <li class="text-white text-lg font-roboto mt-4">Anything redundant and annoying within your business <strong>can</strong> be automated.</li>
+            </ul>
+        </x-gradient-border-card>
+    </div>
+</section>
