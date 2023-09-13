5168d01a1ebd8b767b806fd9e22a85dfdd1acfc8
DamoFD <voshall@yahoo.com>
2023-09-12 19:33:56 -0700

    responsive footer
    responsive footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
-<footer class="bg-brand-gray w-full flex flex-col items-center">
+<footer class="bg-brand-gray w-full flex flex-col items-center mt-56 lg:mt-0">
     <h2 class="text-white font-mont font-extrabold text-center text-3xl">Zenivora</h2>
-    <nav class="flex flex-col items-center w-1/2 mt-6">
-        <ul class="flex items-center space-x-6 mb-6">
+    <nav class="flex flex-col items-center w-11/12 lg:w-1/2 mt-6">
+        <ul class="flex items-center space-x-4 lg:space-x-6 mb-6">
             <li>
                 <img class="w-6" src="{{ asset('svg/facebook.svg') }}" alt="facebook icon">
             </li>
@@ -33,7 +33,7 @@
                 <img class="w-6" src="{{ asset('svg/pinterest.svg') }}" alt="pinterest icon">
             </li>
         </ul>
-        <ul class="flex space-x-10">
+        <ul class="flex space-x-4 lg:space-x-10">
             <li>
                 <a class="text-white underline font-bold font-roboto" href="{{ route('home') }}">
                     Home
