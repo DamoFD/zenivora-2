4613c08d488bc9764c5a42de1b38046e0751b3dd
DamoFD <voshall@yahoo.com>
2023-09-12 19:27:03 -0700

    desktop footer
    desktop footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,65 @@
+<footer class="bg-brand-gray w-full flex flex-col items-center">
+    <h2 class="text-white font-mont font-extrabold text-center text-3xl">Zenivora</h2>
+    <nav class="flex flex-col items-center w-1/2 mt-6">
+        <ul class="flex items-center space-x-6 mb-6">
+            <li>
+                <img class="w-6" src="{{ asset('svg/facebook.svg') }}" alt="facebook icon">
+            </li>
+            <li>
+                <img class="w-6" src="{{ asset('svg/reddit.svg') }}" alt="reddit icon">
+            </li>
+            <li>
+                <img class="w-6" src="{{ asset('svg/tiktok.svg') }}" alt="tiktok icon">
+            </li>
+            <li>
+                <img class="w-6" src="{{ asset('svg/instagram.svg') }}" alt="instagram icon">
+            </li>
+            <li>
+                <img class="w-6" src="{{ asset('svg/linkedin.svg') }}" alt="linkedin icon">
+            </li>
+            <li>
+                <img class="w-6" src="{{ asset('svg/youtube.svg') }}" alt="youtube icon">
+            </li>
+            <li>
+                <img class="w-6" src="{{ asset('svg/x.svg') }}" alt="x icon">
+            </li>
+            <li>
+                <img class="w-6" src="{{ asset('svg/threads.svg') }}" alt="threads icon">
+            </li>
+            <li>
+                <img class="w-6" src="{{ asset('svg/nextdoor.svg') }}" alt="nextdoor icon">
+            </li>
+            <li>
+                <img class="w-6" src="{{ asset('svg/pinterest.svg') }}" alt="pinterest icon">
+            </li>
+        </ul>
+        <ul class="flex space-x-10">
+            <li>
+                <a class="text-white underline font-bold font-roboto" href="{{ route('home') }}">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a class="text-white underline font-bold font-roboto" href="/about">
+                    About
+                </a>
+            </li>
+            <li>
+                <a class="text-white underline font-bold font-roboto" href="/locations">
+                    Locations
+                </a>
+            </li>
+            <li>
+                <a class="text-white underline font-bold font-roboto" href="/Pricing">
+                    Pricing
+                </a>
+            </li>
+            <li>
+                <a class="text-white underline font-bold font-roboto" href="/Contact">
+                    Contact
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <p class="text-sm font-roboto text-gray-200 mt-6">©2023 Zenivora</p>
+</footer>
