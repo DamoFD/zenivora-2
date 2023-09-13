213e3c92d59787372d5204593b2fc233dac092de
DamoFD <voshall@yahoo.com>
2023-09-13 11:50:52 -0700

    created sitemaps
    created sitemaps

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SitemapController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        return response()->view('sitemap.index')->header('Content-Type', 'text/xml');
+    }
 }
