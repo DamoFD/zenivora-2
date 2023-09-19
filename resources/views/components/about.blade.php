4ef3b788b5e853b6026ba6db5e6e12aecc2e1446
pbandjib <pbandjib@gmail.com>
2023-09-19 17:30:37 -0400

    About component made dynamic, Pittsburgh service added surfer content

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,4 +1,4 @@
 <section class="w-full lg:min-h-screen flex justify-center items-center flex-col px-4 lg:px-0" id="about">
-    <h2 class="text-white text-3xl font-mont font-extrabold text-center">Trusted by <strong class="bg-gradient-to-r from-brand-primary to-blue-400 text-transparent bg-clip-text">Major Brands</strong> you know and love</h2>
+    <h2 class="text-white text-3xl font-mont font-extrabold text-center">{!! $information->title !!}</h2>
     <x-marquee />
 </section>
