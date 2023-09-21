c5aa0d4a128ae69ccc48a8440eb4056ee0517ce9
pbandjib <pbandjib@gmail.com>
2023-09-21 15:49:07 -0400

    Location sitemap is dynamic

---
+++ app/Http/Controllers/LocationSitemapController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+use App\Services\ServiceFactory;
+class LocationSitemapController extends Controller
+{
+    public function index(): Response
+    {
+        $locations = ServiceFactory::getLocations();
+        return response()
+            ->view('sitemap.locations.index', compact('locations'))
+            ->header('Content-Type', 'text/xml');
+
+    }
+}
