0f66944f0c1d8e2736788d40eb868ad5f3472503
pbandjib <pbandjib@gmail.com>
2023-09-22 20:30:55 -0400

    random fixes

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,4 +1,4 @@
-<section class="w-full lg:min-h-screen flex justify-center items-center flex-col px-4 lg:px-0" id="about">
+<section class="w-full lg:min-h-screen mt-10 flex justify-center items-center flex-col px-4 lg:px-0" id="about">
     <h2 class="text-white text-3xl font-mont font-extrabold text-center">{!! $information->title !!}</h2>
     <x-marquee />
 </section>
